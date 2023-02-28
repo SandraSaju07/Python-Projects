153ac0a8dc307d6e5097a1705e8051800d6fd81d
Sandy@007 <sandrasaju07@gmail.com>
2023-02-28 23:31:22 +0530

    Initial Version

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+# youtuber = "Sandra"
+
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer Programming is no {adj}! It makes me excited all the time because\
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
